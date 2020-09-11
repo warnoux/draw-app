397ad0797fc75357906c4b2ce287ac7f86d3be8f
WARNOUX <wa95@free.fr>
2020-09-11 16:27:05 +0200

    add picture

--- server/app.js
+++ server/app.js
@@ -2,22 +2,32 @@ const Express = require("express")();
 const Http = require("http").Server(Express);
 const Socketio = require("socket.io")(Http);
 
+const height = 25;
+const width = 25;
 
-const GRID_SIZE = 20;
+const { createCanvas, loadImage } = require('canvas')
+const canvas = createCanvas(height, width)
+const ctx = canvas.getContext('2d')
 
 let matrice= new Array();
 
 Http.listen(3000, () => {
     console.log("Listening at :3000...");
 	
-	for(let i = 0; i < GRID_SIZE; i++){
-	  for(let j = 0; j < GRID_SIZE; j++){
-		if (!matrice[j]) matrice[j] = new Array(); 
-		matrice[j][i] = new Case(i, j, null) ;
-	  }	
-	}
+	// Draw cat with lime helmet
+	loadImage('dl2.jpg').then((image) => {
+	  ctx.drawImage(image, 0, 0, width, height)
+	 
+		for(let i = 0; i < width; i++){ 
+		  for(let j = 0; j < height; j++){
+			   var pixel = ctx.getImageData(i, j, 1, 1);
+            var data = pixel.data;
+			if (!matrice[j]) matrice[j] = new Array(); 
+			matrice[j][i] = new Case(i, j, "rgb("+data[0] + ',' + data[1] + ',' + data[2]+")", null) ;
+		  }
+		}
+	})
 });
-
 
 Socketio.on("connection", socket => {
 	
@@ -28,34 +38,28 @@ Socketio.on("connection", socket => {
 	  },1000);
 	
 	socket.on("changeColor", data => {
-		matrice[data.j][data.i] = new Case(data.i, data.j, 'red');
+			matrice[data.j][data.i] = new Case(data.i, data.j, null, data.color);
+		
     });
 	
 });
-
-	
-function getRandomColor() {
-  var letters = '0123456789ABCDEF';
-  var color = '#';
-  for (var i = 0; i < 6; i++) {
-	color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-}
 
 class Case {
 	i; 
 	j; 
 	color; 
+	selectedColor;
 
 	
-	constructor(i, j, color){
+	constructor(i, j, color, selectedColor){
 		this.i = i;
 		this.j = j;
-		if(color) {
+		if ( color ) {
 			this.color = color;
-		} else {
-			this.color = getRandomColor();
+		}
+		
+		if ( selectedColor != null ) {
+			this.selectedColor = selectedColor;
 		}
 	}
 
