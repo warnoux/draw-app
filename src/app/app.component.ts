7782ed90ac944ff3d4943473a4dfdaec50ca322a
WARNOUX <wa95@free.fr>
2020-09-11 12:49:21 +0200

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
+import io from 'socket.io-client';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  private socket: any;
+  matrice: Array<number>;
+  public ngOnInit() {
+    this.socket = io("http://localhost:3001");
+    this.socket.emit("init");
+  }
+  public ngAfterViewInit() {
+    this.socket.on("matrice", data => {
+      this.matrice = data;
+    });
+  }
+
+  changeColor(col){
+    if ( col.color != "rgb(255,255,255)" && !col.selectedColor)
+      this.socket.emit("changeColor", col);
+  }
+}
